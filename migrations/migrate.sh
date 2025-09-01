0dc92af2b9dad453303d6685561c8853761be988
Antonin-Suzor <antonin@suzor.net>
2025-09-01 16:39:24 +0200

    Zip migration lambda with

--- migrations/migrate.sh
+++
@@ -1,9 +0,0 @@
-function handler () {
-    mig_version=$(cat current_version.txt)
-    echo "mig_version: $mig_version"
-    echo "migrate info:"
-    sqlx migrate info --no-dotenv --source sql
-    echo ""
-    echo "migrate run:"
-    sqlx migrate run --no-dotenv --source sql --target-version $mig_version
-}
